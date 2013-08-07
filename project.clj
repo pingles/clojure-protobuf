f2dac95fd63ce4a5e8068df3717eae3e425d4437
Paul Ingles <paul@oobaloo.co.uk>
2013-08-07 10:05:30 +0100

    release version of lib for use with protobuf 2.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pingles/protobuf "0.7.3-2.5.0-SNAPSHOT"
+(defproject org.pingles/protobuf "0.7.3-2.5.0"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
